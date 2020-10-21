a3d60194ed18cda2990ccbe8b661a7d594f0ef0f
dms-yondy <price1858@gmail.com>
2020-10-21 09:36:22 +0100

    Added BoardEvaluator member, constructor and toString method to MinMax class

--- src/main/java/com/chess/engine/player/ai/MinMax.java
+++ src/main/java/com/chess/engine/player/ai/MinMax.java
@@ -5,8 +5,19 @@ import com.chess.engine.board.Move;
 
 public class MinMax implements MoveStrategy{
 
+    private final BoardEvaluator boardEvaluator;
+
+    public MinMax() {
+        this.boardEvaluator = null;
+    }
+
     @Override
     public Move execute(Board board, int depth) {
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return "MinMax";
     }
 }
