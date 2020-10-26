4703f60d7eff4f043a65b0f2d936f48dca942f02
dms-yondy <price1858@gmail.com>
2020-10-26 06:46:47 +0000

    Added min and max methods in MinMax class

--- src/main/java/com/chess/engine/player/ai/MinMax.java
+++ src/main/java/com/chess/engine/player/ai/MinMax.java
@@ -2,6 +2,7 @@ package com.chess.engine.player.ai;
 
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
+import com.chess.engine.player.MoveTransition;
 
 public class MinMax implements MoveStrategy{
 
@@ -14,6 +15,32 @@ public class MinMax implements MoveStrategy{
     @Override
     public Move execute(Board board, int depth) {
         return null;
+    }
+
+    public int min(final Board board, final int depth) {
+        if(depth == 0) return this.boardEvaluator.evaluate(board, depth);
+        int lowestSeenValue = Integer.MAX_VALUE;
+        for(final Move move : board.getCurrentPlayer().getLegalMoves()) {
+            final MoveTransition moveTransition = board.getCurrentPlayer().makeMove(move);
+            if(moveTransition.getMoveStatus().isDone()) {
+                final int currentValue = max(moveTransition.getTransitionBoard(), depth - 1);
+                lowestSeenValue = Math.min(lowestSeenValue, currentValue);
+            }
+        }
+        return lowestSeenValue;
+    }
+
+    public int max(final Board board, final int depth) {
+        if(depth == 0) return this.boardEvaluator.evaluate(board, depth);
+        int highestSeenValue = Integer.MIN_VALUE;
+        for(final Move move : board.getCurrentPlayer().getLegalMoves()) {
+            final MoveTransition moveTransition = board.getCurrentPlayer().makeMove(move);
+            if(moveTransition.getMoveStatus().isDone()) {
+                final int currentValue = min(moveTransition.getTransitionBoard(), depth - 1);
+                highestSeenValue = Math.max(highestSeenValue, currentValue);
+            }
+        }
+        return highestSeenValue;
     }
 
     @Override
