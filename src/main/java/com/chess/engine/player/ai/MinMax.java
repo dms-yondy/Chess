7e3391c09d47f4f3a73f63c70aa3364cb40e0571
dms-yondy <price1858@gmail.com>
2020-10-26 07:15:17 +0000

    Added execute method to MinMax class

--- src/main/java/com/chess/engine/player/ai/MinMax.java
+++ src/main/java/com/chess/engine/player/ai/MinMax.java
@@ -14,7 +14,32 @@ public class MinMax implements MoveStrategy{
 
     @Override
     public Move execute(Board board, int depth) {
-        return null;
+        final long startTime = System.currentTimeMillis();
+        Move bestMove = null;
+        int highestSeenValue = Integer.MIN_VALUE;
+        int lowestSeenValue = Integer.MAX_VALUE;
+        int currentValue;
+
+        System.out.println(board.getCurrentPlayer() + "THINKING with depth " + depth);
+        int numMoves = board.getCurrentPlayer().getLegalMoves().size();
+
+        for(final Move move : board.getCurrentPlayer().getLegalMoves()) {
+            final MoveTransition moveTransition = board.getCurrentPlayer().makeMove(move);
+            if(moveTransition.getMoveStatus().isDone()) {
+                currentValue = board.getCurrentPlayer().getAlliance().isWhite() ?
+                        min(moveTransition.getTransitionBoard(), depth - 1) :
+                        max(moveTransition.getTransitionBoard(), depth - 1);
+                if(board.getCurrentPlayer().getAlliance().isWhite() && currentValue >= highestSeenValue) {
+                    highestSeenValue = currentValue;
+                } else if(board.getCurrentPlayer().getAlliance().isBlack() && currentValue <= lowestSeenValue){
+                    lowestSeenValue = currentValue;
+                }
+                bestMove = move;
+            }
+        }
+
+        final long executionTime = System.currentTimeMillis();
+        return bestMove;
     }
 
     public int min(final Board board, final int depth) {
